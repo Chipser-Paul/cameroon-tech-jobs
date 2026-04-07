f80824f4ed4c365519d50dbd1f421f412a57cafd
Fokoua Paul Emmanuel <chipseremmanuel@gmail.com>
2026-04-07 14:13:50 +0100

    Serve media files in production

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +11,8 @@ urlpatterns = [
     path('', include('companies.urls')),
     path('', include('seekers.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += [
+        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
